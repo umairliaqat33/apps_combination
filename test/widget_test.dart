588ee3651a5d9353352f345862ba6536b39d231d
umair <umairliaqat552>
2022-02-19 22:55:11 +0500

    Initial commit

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:apps_combination/main.dart';
+import 'package:apps_combination/Weathermain.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
